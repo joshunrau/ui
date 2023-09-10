5b3f91a951db7262b018aff296335815f82b7fec
joshunrau <contact@joshuaunrau.com>
2023-09-10 14:09:15 -0400

    add example

---
+++ src/components/Example/Example.stories.tsx
@@ -0,0 +1,11 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Example } from './Example';
+
+const meta: Meta<typeof Example> = { component: Example };
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {}
