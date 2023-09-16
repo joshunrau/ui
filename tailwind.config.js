0a75838a407bfaa41793e366e019386312ea6109
joshunrau <contact@joshuaunrau.com>
2023-09-16 15:35:54 -0400

    narrow type

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 // @ts-check
 
-import path from 'path'
+import path from 'path';
 
 /**
  * Note, using `__dirname` is a hack which will work when run with bun (e.g., vite dev server)
- * and also when resolved as a CommonJS module by storybook. Once Bun has implemented the 
+ * and also when resolved as a CommonJS module by storybook. Once Bun has implemented the
  * necessary Node libraries, this can be replaced by `import.meta.dir`.
  */
 
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config & { content: any[] }} */
 export default {
   content: [path.resolve(__dirname, './src/**/*.{js,jsx,ts,tsx}')],
   theme: {
