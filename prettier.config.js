9d34c61ef13f33df24f49c0adebd696712463a82
joshunrau <contact@joshuaunrau.com>
2023-09-16 14:14:14 -0400

    ch prettier

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Options} */
+export default {
+  plugins: ['prettier-plugin-tailwindcss'],
+  htmlWhitespaceSensitivity: 'ignore',
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: 'none'
+};
