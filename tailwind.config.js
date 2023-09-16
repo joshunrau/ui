33978d574ec53ad62646485b4e7e687b42c89e6d
joshunrau <contact@joshuaunrau.com>
2023-09-16 15:29:02 -0400

    add note

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 
 import path from 'path'
 
+/**
+ * Note, using `__dirname` is a hack which will work when run with bun (e.g., vite dev server)
+ * and also when resolved as a CommonJS module by storybook. Once Bun has implemented the 
+ * necessary Node libraries, this can be replaced by `import.meta.dir`.
+ */
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [path.resolve(__dirname, './src/**/*.{js,jsx,ts,tsx}')],
