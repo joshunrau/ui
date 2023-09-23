d7d86f1bd6e1540b733fe1c40f3e66636bfec629
joshunrau <contact@joshuaunrau.com>
2023-09-23 11:59:00 -0400

    use relative import

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -1,7 +1,8 @@
 import { MoonIcon, SunIcon } from '@heroicons/react/24/outline';
 import { twMerge } from 'tailwind-merge';
 
-import { useTheme } from '@/hooks/useTheme';
+// import { useTheme } from '@/hooks/useTheme';
+import { useTheme } from '../../hooks/useTheme';
 
 export type ThemeToggleProps = {
   className?: string;
