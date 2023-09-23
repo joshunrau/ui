7aae696240d263bd2641eac211517e6ff2c61870
joshunrau <contact@joshuaunrau.com>
2023-09-23 12:23:43 -0400

    add theme switcher

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,19 @@
-import type { Preview } from '@storybook/react';
+import { withThemeByDataAttribute } from '@storybook/addon-themes';
+import type { Preview, ReactRenderer } from '@storybook/react';
 
 import './tailwind.css';
 
 const preview: Preview = {
+  decorators: [
+    withThemeByDataAttribute<ReactRenderer>({
+      attributeName: 'data-mode',
+      defaultTheme: 'light',
+      themes: {
+        light: 'light',
+        dark: 'dark'
+      }
+    })
+  ],
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
