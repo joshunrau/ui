a2dc2fc5fd8fd363dd61c3b7491ed1be70a8e448
joshunrau <contact@joshuaunrau.com>
2023-10-01 17:03:49 -0400

    add card

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { cn } from '@/utils';
+
+export const Card = ({ children, className, ...props }: React.HTMLProps<HTMLDivElement>) => (
+  <div className={cn('rounded-md bg-slate-50 p-4 shadow ring-1 ring-slate-900/5 dark:bg-slate-800', className)} {...props}>
+    {children}
+  </div>
+);
