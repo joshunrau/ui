a6d8770e60be37793b768ac501c82db933a6c58f
joshunrau <contact@joshuaunrau.com>
2024-02-09 20:53:13 -0500

    add checkbox

---
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Checkbox } from './Checkbox';
+
+type Story = StoryObj<typeof Checkbox>;
+
+export default { component: Checkbox } as Meta<typeof Checkbox>;
+
+export const Default: Story = {
+  decorators: [
+    (Story) => {
+      return (
+        <div className="flex items-center space-x-2">
+          <Story id="terms" />
+          <label
+            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            htmlFor="terms"
+          >
+            Accept terms and conditions
+          </label>
+        </div>
+      );
+    }
+  ]
+};
